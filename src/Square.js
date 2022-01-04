573a3621f15a3e40d15c832f71e6eece9eaec9f3
devgraphy <beaican530@gmail.com>
2022-01-04 15:24:02 +0900

    Add function: state update on click

--- src/Square.js
+++ src/Square.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 class Square extends React.Component {
+    // component에 state를 이용하여 기억하기
+        // 클래스에 생성자를 통해 state를 초기화
+    constructor(props){
+        super(props);
+        this.state = {
+            value : null,
+        };
+    }
+
     render() {
         return(
             // arrow function
+            // [설명] this.setState를 호출하여 button 클릭 시 Square가 다시 렌더링해야 한다고 알림
             <button className="square" onClick={()=>{
-                console.log('click');
+                // [개념] 컴포넌트에서 setState를 호출하면 React는 자동으로 컴포넌트 내부의 자식 컴포넌트 역시 업데이트한다.
+                this.setState({value: 'X'})
             }}>
                 {this.props.value}
             </button>
